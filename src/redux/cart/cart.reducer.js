ec8df73d518debd5f9e7a1a4c5c83d2e2a319158
cypherpoll-0 <ayushkedia1990@gmail.com>
2022-12-25 16:30:10 +0530

    cart changes and product page changes -ak

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -28,7 +28,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter(
-          (cartItem) => cartItem.id !== action.payload.id
+          (cartItem) => cartItem.product_id !== action.payload.product_id
         ),
       };
     default:
